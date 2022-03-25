a88b59792c919a58e677c1fb0fefb558b2bd745a
gleicepicciani <gleice.picciani@gmail.com>
2022-03-24 21:43:48 -0400

    Seeding no Banco de Dados

--- backend/src/main/java/br/edu/ifms/ordem/resources/TecnicoResource.java
+++ backend/src/main/java/br/edu/ifms/ordem/resources/TecnicoResource.java
@@ -1,26 +1,28 @@
 package br.edu.ifms.ordem.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifms.ordem.entities.Tecnico;
+import br.edu.ifms.ordem.services.TecnicoService;
 
 //controlador - vai processar tudo o q for enviado para a api
 @RestController
 @RequestMapping(value = "/tecnicos") //padrão do recurso - mapeamento para as requisições
 public class TecnicoResource {
 	
+	@Autowired
+	private TecnicoService service;
+	
+	
 	@GetMapping	
 	public ResponseEntity<List<Tecnico>> findAll(){//ele empacota 
-		List<Tecnico> list = new ArrayList<>();		
-		list.add(new Tecnico(1L, "Ana Souza", "(67) 98746-8942", "ana@gmail.com", "123"));
-		list.add(new Tecnico(1L, "Carlos Almeida", "(67) 98978-4542", "carlos@gmail.com", "124"));
-		
+		List<Tecnico> list = service.findAll();			
 		//vai criar um pacote http 
 		return ResponseEntity.ok().body(list);
 		
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_TECNICO (nome, telefone, email, senha) VALUES ('Gleice', '7852235', 'gleice@gmail.com', '1234');
+INSERT INTO TB_TECNICO (nome, telefone, email, senha) VALUES ('Ana', '7888235', 'ana@gmail.com', '1294');
+INSERT INTO TB_TECNICO (nome, telefone, email, senha) VALUES ('José', '7895235', 'jose@gmail.com', '1734');
